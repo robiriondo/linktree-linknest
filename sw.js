87428471be12780f91b2fa78194bb0fff8510b69
Roberto <contact@robertoiriondo.com>
2024-07-30 22:12:51 -0400

    added PWA

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pwa-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'https://www.robertoiriondo.com/assets/images/hero.jpg',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
